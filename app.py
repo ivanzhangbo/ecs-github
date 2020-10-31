66b8a99666f2664d7cc1f1031f7bdecc17d2d6c9
Your Name <you@example.com>
2020-10-31 18:36:51 +0900

    fff

--- app.py
+++ app.py
@@ -2,10 +2,10 @@
 from flask import Flask
 app = Flask(__name__)
 # aaaa
-@app.route('/<random_string>')
-def returnBackwardsString(random_string):
+@app.route('/<random_string1>')
+def returnBackwardsString(random_string1):
     """Reverse and return the provided URI"""
-    return "".join(reversed(random_string))
+    return "".join(reversed(random_string1))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
