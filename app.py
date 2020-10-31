7c19f9ac97166bdac5953ec24cebd10f3ff2a0b6
ivanzhangbo@yahoo.co.jp <you@example.com>
2020-10-31 21:12:42 +0900

    update 2

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ def returnBackwardsString(random_string1):
     """Reverse and return the provided URI"""
     return "".join(reversed(random_string1))
 
+@app.route('/hello')
+def hello():
+   return "Hello AWS-Github!"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
